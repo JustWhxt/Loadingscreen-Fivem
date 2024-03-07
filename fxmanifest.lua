b7200ebfc016e9242612a8f0186afd0a27b2b6bc
Thibaud <141267138+JustWhxt@users.noreply.github.com>
2024-03-07 16:23:48 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Yukaii'
+version '1.0'
+description 'Loadingscreen'
+lua54 'yes'
+loadscreen 'load.html'
+
+shared_script 'config.lua'
+
+client_script 'client/client.lua'
+
+files { 
+    'load.html',
+    './js/main.js', 
+    './css/styles.css',
+    'loadscreen.png',
+    'music.mp3'
+}
